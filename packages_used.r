31ccc726fdbc75432769a612de3f50000d5db25b
Annick Eudes Jean-Baptiste <annickeudes@gmail.com>
2018-08-01 21:05:56 -0400

    Create packages_used.r

---
+++ packages_used.r
@@ -0,0 +1,18 @@
+# Packages used in my analysis
+# to update after an new R version download
+
+install.packages("gglot")
+install.packages("ggplot2")            
+install.packages("AER")
+install.packages("apt")
+install.packages("calibrate")
+install.packages("cluster")
+install.packages("dbplyr")
+install.packages("dplyr")
+install.packages("devtools")
+install.packages("dslabs")
+install.packages("erer")
+install.packages("xlsx")               
+install.packages("calibrate")           
+install.packages("reshape2")           
+install.packages("ggpubr")
