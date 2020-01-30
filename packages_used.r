bee826305ad9bb9b0392278054c8772e18938b20
Annick Eudes Jean-Baptiste <annickeudes@gmail.com>
2020-01-30 09:56:33 -0500

    Update packages_used.r

--- packages_used.r
+++ packages_used.r
@@ -1,25 +1,25 @@
 
 # Packages used in my analysis
 # to update after an new R version download
-# List last updated oct 2018
+# List last updated Jan 2020
 
-install.packages("gglot")
-install.packages("ggplot2")            
+# install.packages("gglot")
+# install.packages("ggplot2")            
 install.packages("AER")
 install.packages("apt")
 install.packages("calibrate")
 install.packages("cluster")
-install.packages("dbplyr")
-install.packages("dplyr")
-install.packages("stringr")
+# install.packages("dbplyr")
+# install.packages("dplyr")
+# install.packages("stringr")
 install.packages("devtools")
-install.packages("dslabs")
-install.packages("erer")
+install.packages("dslabs") # course
+install.packages("erer") # Empirical Economics
 install.packages("xlsx")               
-install.packages("calibrate")           
+# install.packages("calibrate")           
 install.packages("reshape2")           
 install.packages("ggpubr")
-install.packages("ggmap")
+install.packages("ggmap") # Cartes
 install.packages("mapproj")
 install.packages("RCurl")
 install.packages("lubridate")   # date formats
@@ -34,7 +34,7 @@ install.packages("Epi")
 # Intervalle à 95% d'un pourcentage 
 install.packages("binom")
 library(binom)
-install.packages("gplots", dep=TRUE)
+# install.packages("gplots", dep=TRUE)
 
 # These packages are used to Map Provincial Map using GADM
 install.packages("maptools", dependencies = TRUE)
@@ -43,12 +43,21 @@ install.packages("raster")
 # Text analysis
 install.packages("wordcloud")
 install.packages("RColorBrewer")
-install.packages("tm")
+install.packages("tm") # text mining
 install.packages("SnowballC")
 install.packages("devtools")
-install.packages(c("e1071", "caret", "quanteda", 
-                   "irlba", "randomForest"))
-
+install.packages(c("e1071", "caret", "quanteda", "irlba", "randomForest"))
 install.packages("tidytext")
-
 install.packages(c("DT", "knitr"))  
+
+# Other packages form work related projects
+install.packages("pander")
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("frequencies")
+install.packages("psych")
+install.packages("stringr")
+install.packages("lubridate")
+install.packages("knitr")
+install.packages("lemon")
+install.packages("pander")
+install.packages("rbokeh")
